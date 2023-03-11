a64f413182e656300b50689fb7f175ba7829ef31
Mantrel <mantrel_yt@hotmail.com>
2023-03-11 18:51:33 -0300

    correccion

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className="imgHome">
-      <h1>Bienvenido maestro pokemón</h1>
-      <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c325.png" alt="IconoHome"/>
-    </div>
+    
+      <div className="imgHome">
+        <h1>Bienvenido maestro pokemón</h1>
+        <img src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c325.png' alt="IconoHome" />
+      </div>
+    
   )
 }
 
